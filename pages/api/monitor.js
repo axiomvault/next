437c0a703048fd9f591cd8b08ac8790a4385fd73
axiomvault <dikshasingh009977@gmail.com>
2025-08-13 02:34:25 +0530

    erc fit9999

--- pages/api/monitor.js
+++ pages/api/monitor.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
+
     if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
